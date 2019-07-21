e38cbe20b7368cdc7a442484dce3dded6feba028
cingo85 <cingo85@gmail.com>
2019-07-21 18:49:23 +0800

    jack
    wip

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>INSPINIA | Login</title>
+    <style src="../assets/css/bootstrap.min.css"></style>
+    <style src="../assets/css/animate.css"></style>
+    
+  
+
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
+
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+</head> 
+</template>
+
+<script>
+export default {
+    name:loginHeader
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,45 @@
+<template>
+ 
+    <body class="gray-bg">
+
+    <div class="middle-box text-center loginscreen animated fadeInDown">
+        <div>
+            <div>
+
+                <h6 class="logo-name">IN+</h6>
+
+            </div>
+            <h3>ETL清洗程式</h3>
+           
+            <p>請輸入帳號密碼</p>
+            <form class="m-t" role="form" action="index.html">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Username" required="" id="username">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" placeholder="Password" required="" id="password">
+                </div>
+                <button type="submit" class="btn btn-primary block full-width m-b">Login</button>
+
+                <a class="btn btn-sm btn-white btn-block" href="register.html">Create an account</a>
+            </form>
+        </div>
+    </div>
+
+</body>
+</template>
+
+<script>
+export default {
+    name: 'login'
+
+}
+
+   
+   
+</script>
+
+
+<style>
+
+</style>
